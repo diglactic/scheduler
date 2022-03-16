136bf31eab2236c378a7520da7f7d5e97e39ab07
Sheng-Liang Slogar <sheng@diglactic.com>
2022-03-16 02:49:30 -0600

    Oops

--- apps/web/components/booking/pages/BookingPage.tsx
+++ apps/web/components/booking/pages/BookingPage.tsx
@@ -327,7 +327,7 @@ const BookingPage = ({ eventType, booking, profile }: BookingPageProps) => {
                   </p>
                 )}
                 {/*eslint-disable-next-line*/}
-                <p className="mb-8 text-gray-600 dark:text-white whitespace-pre-line">{props.eventType.description}</p>
+                <p className="mb-8 text-gray-600 dark:text-white whitespace-pre-line">{eventType.description}</p>
               </div>
               <div className="sm:w-1/2 sm:pl-8 sm:pr-4">
                 <Form form={bookingForm} handleSubmit={bookEvent}>
